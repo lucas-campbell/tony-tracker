a16d388583f26e1ec50b7fba0c29e5c1598864de
Lucas Campbell <lucas.campbell197@gmail.com>
2017-10-14 12:23:42 -0400

    added database addon

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ app.get('/', function(request, response) {
     response.render('pages/index')
 });
 
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+
+// connection.end();

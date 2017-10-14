9fb21e74d1710d7a4f401d37444c9013ebcec7e9
Lucas Campbell <lucas.campbell197@gmail.com>
2017-10-14 12:03:02 -0400

    it's fucking live

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.get('/', function(request, response) {
     response.render('pages/index')
 });
 
-
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });

6bd1a5d0807167d93e7e9f99630fe763ddd2c994
Lucas Campbell <lucas.campbell197@gmail.com>
2017-11-13 17:02:59 -0500

    move mysql test again

---
+++ public/mysqltest.js
@@ -0,0 +1,10 @@
+/* mysql test */
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection.connect(function(err) {
+if (err) {
+        console.error('error connecting', + err.stack);
+        return;
+}
+console.log('connected as id', + connection.threadId);
+});
